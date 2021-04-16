a2a3b4e42efbaa67964d829fd6c021e84b51181f
julianegoe <goersch.juliane@dpa.com>
2021-04-16 18:46:19 +0200

    added propTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
     render() {
@@ -8,10 +9,31 @@ export class MovieCard extends React.Component {
             <img className="movie-poster" src={movieData.ImagePath} alt="movie poster" />
             <div className="movie-info">
                 <p>{"Year: " + movieData.Year}</p>
-                <p>{"Director: " + movieData.Director}</p>
-
+                <p>{"Director: " + movieData.Director.Name}</p>
             </div>
         </div >
 
     }
+}
+
+MovieCard.propTypes = {
+    movieData: PropTypes.exact({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string
+        }).isRequired,
+        Year: PropTypes.number.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }).isRequired,
+        Actors: PropTypes.array.isRequired,
+        _id: PropTypes.string.isRequired,
+        ImageUrl: PropTypes.string.isRequired,
+        Featured: PropTypes.bool.isRequired
+    }).isRequired,
+    onClick: PropTypes.func
 }
